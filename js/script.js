f7ebb5f58ef76de06f53b8699de7897fd0f19eb5
Luis <luis.fernando3893@hotmail.com>
2020-02-07 09:49:10 -0300

    Adicionando cometários ao projeto calculadora

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let tela = document.getElementById("visor");
+let tela = document.getElementById("visor"); //Local onde exibe os dados
 
 //o evento de click esta no div que esta o numero/simbolo
-const adicionaValor = valor => tela.innerText+=valor;
-const limpaTela = () => tela.innerText="";
-const somaVisor = () => tela.innerText = eval(tela.innerText);
+const adicionaValor = valor => tela.innerText += valor; // Adiciona o valor na tela
+const limpaTela = () => tela.innerText = ""; // Limpa tela
+const somaVisor = () => tela.innerText = eval(tela.innerText); //Executa a operação matemática
